3b3590e550537a58f26f9e1d532895a406b82345
Debangan Bhattacharyya <112092614+debangan03@users.noreply.github.com>
2024-06-15 03:56:42 +0530

    Update ResetPasswordPage.jsx

--- src/pages/ResetPasswordPage.jsx
+++ src/pages/ResetPasswordPage.jsx
@@ -1,66 +1,10 @@
-import React, { useState } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { toast, Toaster } from "react-hot-toast";
+import React from 'react'
 
 function ResetPasswordPage() {
-  const { token } = useParams();
-  const navigate = useNavigate();
-  const [newPassword, setNewPassword] = useState("");
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const { data } = await axios.post(
-        `https://skillearner-server-1.onrender.com/resetpassword`,
-        {
-          token,
-          newPassword,
-        }
-      );
-      if (data.success) {
-        toast.success("success");
-        navigate("/login");
-      } else {
-        throw error("Invalid");
-      }
-    } catch (error) {
-      toast.error("error.response.data.message");
-    }
-  };
+  
 
   return (
-    <div className="min-h-screen flex flex-col items-center p-4 bg-gray-100">
-      <div className="max-w-md w-full bg-white mt-20 p-8 shadow-lg rounded-lg">
-        <h1 className="text-3xl font-bold mb-6 text-center">Reset Password</h1>
-        <form onSubmit={handleSubmit}>
-          <Toaster />
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="newPassword"
-            >
-              New Password
-            </label>
-            <input
-              type="password"
-              id="newPassword"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          >
-            Reset Password
-          </button>
-        </form>
-      </div>
-    </div>
+    <div>resetpassword</div>
   );
 }
 
